6cd30377c1de53a3ff408181e30d5f6df77dc184
emmy-studios <emmystudios@proton.me>
2024-10-02 02:31:57 -0600

    "Add vuetify"

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,6 +7,27 @@
         <h1>About Us</h1>
     </div>
 
+    <v-container>
+
+        <v-row justify="center">
+
+            <v-card 
+                title="This is the title"
+                text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, consequatur.</">
+            </v-card>
+
+        </v-row>
+
+    </v-container>
+
+    <v-container>
+        <v-row justify="center">
+            <v-btn>
+                Button
+            </v-btn>
+        </v-row>
+    </v-container>
+
 </template>
 
 <style scoped>
@@ -15,6 +36,7 @@
         display: flex;
         justify-content: center;
         margin-top: 16px;
+        margin-bottom: 20px;
     }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+
+// Vue Router
 import router from './plugins/router';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+  
+
+// Create App
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 app.mount('#app');
